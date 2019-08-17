d65e7feb3667cca45a41f7e861cfca705ab6fc59
keemsisi <keemsisi@gmail.com>
2019-08-17 00:09:04 -0700

    commited

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-<<<<<<< HEAD
 import 'hammerjs';
-=======
->>>>>>> 3ef147db79ca826e40a81226191d60342b05183b
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
